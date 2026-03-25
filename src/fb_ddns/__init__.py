7e6a313d650f23dfa88ec0580f6142b8bc7ae8aa
Frank Brehm <frank@brehm-online.com>
2026-03-25 10:43:06 +0100

    Adding CHANGELOG.md and src/fb_ddns/__init__.py

---
+++ src/fb_ddns/__init__.py
@@ -0,0 +1,18 @@
+#!/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+@summary: A package of modules and scripts for managing dynamic DNS clients.
+
+@author: Frank Brehm
+@contact: frank@brehm-online.com
+@copyright: © 2026 by Frank Brehm, Berlin
+"""
+
+from __future__ import absolute_import
+
+DDNS_CFG_BASENAME = "ddns.ini"
+MAX_TIMEOUT = 3600
+
+__version__ = "0.1.0"
+
+# vim: ts=4 et list
